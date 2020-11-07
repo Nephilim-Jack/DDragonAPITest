226fefc616800fe8545521ecc45d21a0a99c6345
Matheus Mendes <ronaldo_rogerio@yahoo.com.br>
2020-11-07 13:51:51 -0300

    feat: testing api, debug only purpose

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import { WelcomeMessage } from './styles'
-import { getSplashArtUrlOf } from '../../services/ddragon/api'
+import { getSplashArtUrlOf, getDetailOf } from '../../services/ddragon/api'
+import { ChampionData } from '../../services/ddragon/interfaces'
+
 
 const HomePage = () => {
-    const champion = 'Nasus'
-    const [championImage, setChampionImage] = useState<any>()
+    const [championName, setChampionName] = useState('')
+    const [championData, setChampionData] = useState<ChampionData | any>({})
+    const [championImage, setChampionImage] = useState<string>('')
+
+    const loadChampionData = async () => {
+        const champData = (await getDetailOf(championName)).data
+        setChampionData(champData)
+    }
+
+    const handleInputChange = (event: any) => {
+        if (event.key === 'Enter') {
+            if (championName !== event.target.value) {
+                setChampionName(event.target.value)
+            }
+        }
+    }
+
     useEffect(() => {
-        setChampionImage(getSplashArtUrlOf(champion))
-    }, [])
+        setChampionImage(getSplashArtUrlOf(championName))
+        loadChampionData()
+    }, [championName])
 
     return (
-        <>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
             <WelcomeMessage>Data Dragon Api Test</WelcomeMessage>
-            {championImage ? <img src={championImage} alt={`Imagem do(a) ${champion}`} /> : null}
-
-        </>
+            <input type="text" onKeyPress={event => { handleInputChange(event) }} />
+            {championImage ? <img src={championImage} alt={`Imagem do(a) ${championName}`} width="100%" /> : null}
+            {championData ? <p>{championData.lore}</p> : null}
+        </div>
     )
 }
 // on main
